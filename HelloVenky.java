d6b7ff2cfc7a70f33b718420be77992dfe027d62
Venkatesh2806 <venkateshwalunj2806@gmail.com>
2022-09-28 10:09:13 +0530

    New Java file is added

---
+++ HelloVenky.java
@@ -0,0 +1,11 @@
+
+public class HelloVenky {
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+		int a=1;
+System.out.println(a);
+if(a==1);
+System.out.println("Wellcome to Java");
+	}
+
+}
